378c7bf394673dc57094724a9bd9cf706fcf28ac
anfdrva <angelina0403@ukr.net>
2023-10-27 22:53:41 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,62 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+
+// export {};
+
+
+class Key{
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number{
+        return this.signature;
+    }
+}
+
+class Person{
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House{
+    protected tenants: Person[];
+    protected door: boolean;
+
+    constructor(protected key: Key) {
+        this.key = key;
+    }
+
+    comeIn(newTenant: Person) {
+        if (this.door) {
+            this.tenants.push(newTenant);
+        }
+    }
+
+    abstract openDoor(keyToEnterTheHouse: Key): void;
+}
+
+class MyHouse extends House{
+    openDoor(keyToEnterTheHouse: Key): void {
+        if (this.key.getSignature() === keyToEnterTheHouse.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +66,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { };
